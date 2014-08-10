12b0fd14ed494a4d179a2b6a4e2224cdf4067cc1
florian <bacle.florian@gmail.com>
2014-08-10 22:48:44 +0200

    clean frontend bundle

--- src/fibe/FrontendBundle/Resources/public/app/js/controllers.js
+++ src/fibe/FrontendBundle/Resources/public/app/js/controllers.js
@@ -16,7 +16,7 @@ liveconControllers.controller('mainCtrl', ['$scope', '$routeParams', 'GLOBAL_CON
 
 
 
-/*********************************** NAVS **********************************************/
+/*********************************** ALERT **********************************************/
 liveconControllers.controller('AlertCtrl', ['$scope', '$routeParams', 'GLOBAL_CONFIG', '$timeout',
     function ($scope, $routeParams, GLOBAL_CONFIG, $timeout) {
         $scope.alerts = [];
@@ -78,69 +78,14 @@ liveconControllers.controller('navTopCtrl', ['$translate','$scope', '$routeParam
 
     }]);
 
-/*********************************** PUBLICATIONS **********************************************/
-liveconControllers.controller('publicationsCtrl', ['$scope', '$rootScope', 'publicationFactory', 'GLOBAL_CONFIG',
-    function ($scope, $rootScope, publicationFactory, GLOBAL_CONFIG) {
-        $scope.publications = publicationFactory.list({conferenceId: GLOBAL_CONFIG.conferenceId});
-        $scope.orderProp = 'age';
-        $rootScope.title = "Publications";
-    }]);
-
-
-liveconControllers.controller('publicationCtrl', ['$scope', '$rootScope', '$routeParams', 'publicationFactory',
-    function ($scope, $rootScope, $routeParams, publicationFactory) {
-        publicationFactory.show({publicationId: $routeParams.publicationId}, function (publication) {
-            $scope.publication = publication[0];
-            $rootScope.title = publication[0].title;
-        });
-    }]);
-
-
-/*********************************** PERSONS **********************************************/
-liveconControllers.controller('personsCtrl', ['$scope', '$rootScope', '$routeParams', 'personFactory', 'GLOBAL_CONFIG',
-    function ($scope, $rootScope, $routeParams, personFactory, GLOBAL_CONFIG) {
-        personFactory.list({conferenceId: GLOBAL_CONFIG.conferenceId}, function (persons) {
-            $scope.persons = persons;
-            $rootScope.title = "Speakers";
-        });
-    }]);
-
-liveconControllers.controller('personCtrl', ['$scope', '$rootScope', '$routeParams', 'personFactory',
-    function ($scope, $rootScope, $routeParams, personFactory) {
-        personFactory.show({personId: $routeParams.personId}, function (person) {
-            $scope.person = person[0];
-            $rootScope.title = person[0].name;
-        });
-    }]);
-
-
-/*********************************** CONFERENCE **********************************************/
-liveconControllers.controller('conferenceCtrl', ['$scope', '$rootScope', '$routeParams', 'conferenceFactory', 'GLOBAL_CONFIG',
-    function ($scope, $rootScope, $routeParams, conferenceFactory, GLOBAL_CONFIG) {
-        $scope.phone = conferenceFactory.show({conferenceId: GLOBAL_CONFIG.conferenceEventId}, function (conference) {
-            $scope.conference = conference[0];
-            $rootScope.title = conference[0].name;
-        });
-    }]);
-
-
-
 /*********************************** DASHBOARDS **********************************************/
-liveconControllers.controller('dashboardCtrl', ['$scope', '$rootScope', '$routeParams', 'organizationFactory', 'GLOBAL_CONFIG',
-    function ($scope, $rootScope, $routeParams, organizationFactory, GLOBAL_CONFIG) {
+liveconControllers.controller('dashboardCtrl', ['$scope', '$rootScope', '$routeParams', 'GLOBAL_CONFIG',
+    function ($scope, $rootScope, $routeParams, GLOBAL_CONFIG) {
 
     }]);
 
-/*********************************** WIDGETS **********************************************/
-liveconControllers.controller('widgetCtrl', ['$scope', '$rootScope', '$routeParams', 'organizationFactory', 'GLOBAL_CONFIG',
-    function ($scope, $rootScope, $routeParams, organizationFactory, GLOBAL_CONFIG) {
+/*********************************** HOME **********************************************/
+liveconControllers.controller('conferenceCtrl', ['$scope', '$rootScope', '$routeParams', 'GLOBAL_CONFIG',
+    function ($scope, $rootScope, $routeParams, GLOBAL_CONFIG) {
 
     }]);
-
-
-/*********************************** SCHEDULE **********************************************/
-liveconControllers.controller('scheduleCtrl', ['$scope', '$rootScope', '$routeParams', 'organizationFactory', 'GLOBAL_CONFIG',
-    function ($scope, $rootScope, $routeParams, organizationFactory, GLOBAL_CONFIG) {
-
-    }]);
-
