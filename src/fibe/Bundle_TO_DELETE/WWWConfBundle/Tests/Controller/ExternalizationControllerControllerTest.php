f8aec8623d10cddf7d55ae8337fd115bfde9754a
Benoit <benoitddlp@gmail.com>
2014-08-19 19:50:04 +0200

    clean tests

--- src/fibe/Bundle_TO_DELETE/WWWConfBundle/Tests/Controller/ExternalizationControllerControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace fibe\Bundle\WWWConfBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class ExternalizationControllerControllerTest extends WebTestCase
-{
-}
--- src/fibe/EventBundle/Tests/Controller/ModuleControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace fibe\EventBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class ModuleControllerTest extends WebTestCase
-{
-}
