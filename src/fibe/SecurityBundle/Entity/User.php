0c70e034476700942f9a9b2c23b6bd3c7f257b55
Vincent <vincent.sebille@gmail.com>
2014-10-04 17:34:10 +0200

    [V2][fix] fix deprecated user bundle

--- src/fibe/SecurityBundle/Entity/User.php
+++ src/fibe/SecurityBundle/Entity/User.php
@@ -5,7 +5,7 @@
   use Doctrine\Common\Collections\ArrayCollection;
   use fibe\CommunityBundle\Entity\Person;
   use fibe\EventBundle\Entity\MainEvent;
-  use FOS\UserBundle\Entity\User as BaseUser;
+  use FOS\UserBundle\Model\User as BaseUser;
   use Doctrine\ORM\Mapping as ORM;
 
   use JMS\Serializer\Annotation\Exclude;
