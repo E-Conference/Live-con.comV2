a77437ac94432809ed8af4e1ab2688a32568a1b8
Benoit <benoitddlp@gmail.com>
2014-10-11 04:14:36 +0200

    refacto get or create + front searchService POC

--- src/fibe/FrontendBundle/Resources/public/app/modules/authentication/js/services.js
+++ src/fibe/FrontendBundle/Resources/public/app/modules/authentication/js/services.js
@@ -11,23 +11,22 @@
  * @type {factory}
  */
 angular.module('authenticationApp').factory('usersFact', ['$resource',
-  function ($resource)
-  {
-    return $resource(
-      globalConfig.api.urls.login,
-      {'id': '@id'},
-      {
-        get: {method: 'GET', params: {}, isArray: false},
-        signin: {method: 'POST', params: {}, isArray: false},
-        signup: {method: 'POST', params: {no_clean:true}, url: globalConfig.api.urls.registration, isArray: false},
-        signout: {method: 'GET', url: globalConfig.api.urls.logout, isArray: false},
-        confirm: {method: 'POST', url: globalConfig.api.urls.confirm, isArray: false},
-        changepwd: {method: 'POST', url: globalConfig.api.urls.changepwd, isArray: false},
-        resetpwdrequest: {method: 'POST', url: globalConfig.api.urls.reset_pwd_request, isArray: false},
-        resetpwd: {method: 'POST', url: globalConfig.api.urls.reset_pwd, isArray: false},
-        show: {method: 'GET', isArray: false},
-        list: {method: 'GET', url: globalConfig.api.urls.organizations + '.json', params: {}, isArray: true}
-      }
-    );
-  }]);
+                                                          function ($resource)
+                                                          {
+                                                              return $resource(
+                                                                  globalConfig.api.urls.login,
+                                                                  {id: '@id'},
+                                                                  {
+                                                                      get            : {method: 'GET', params: {}, isArray: false},
+                                                                      signin         : {method: 'POST', params: {}, isArray: false},
+                                                                      signup         : {method: 'POST', params: {no_clean: true}, url: globalConfig.api.urls.registration, isArray: false},
+                                                                      signout        : {method: 'GET', url: globalConfig.api.urls.logout, isArray: false},
+                                                                      confirm        : {method: 'POST', url: globalConfig.api.urls.confirm, isArray: false},
+                                                                      changepwd      : {method: 'POST', url: globalConfig.api.urls.changepwd, isArray: false},
+                                                                      resetpwdrequest: {method: 'POST', url: globalConfig.api.urls.reset_pwd_request, isArray: false},
+                                                                      resetpwd       : {method: 'POST', url: globalConfig.api.urls.reset_pwd, isArray: false},
+                                                                      show           : {method: 'GET', isArray: false}
+                                                                  }
+                                                              );
+                                                          }]);
 
